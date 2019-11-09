8056558594167ff9346f1c941b7f0727a9847569
暴躁DPS <13552960783@163.com>
2019-11-09 20:57:22 +0800

    Update entrypoint.sh

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
 echo ${GITHUB_SHA}
-echo $URL
-curl $URL+'?text='+$MESSAGE+'\&desp='+${GITHUB_SHA}
+echo $URL我嗯嗯无多多
+curl www.baidu.com
+ping www.baidu.com
 echo '👍 GREAT SUCCESS!'
